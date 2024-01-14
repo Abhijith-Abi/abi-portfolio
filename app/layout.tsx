3f026e28ae2f5c9440732cb8c73ed420033c0519
abhijith-pa <abhijith@steyp.com>
2024-01-14 16:53:44 +0530

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    metadataBase: new URL(
-        "abi-portfolio-cdyl1ibi8-abhijith-s-projects.vercel.app"
-    ),
+    // metadataBase: new URL(
+    //     "https://developer-portfolio-ibrahim-memons-projects.vercel.app"
+    // ),
     title: "Abhijith Portfolio",
     description: "Developer Portfolio By Abhijith P A",
     keywords: ["Developer", "Portfolio", "Developer Portflio", "Abhijith P A"],
