a77a16b6e27728e9f7aa58231ad71a6559061fd8
abhijith-pa <abhijith@steyp.com>
2024-01-14 14:06:59 +0530

    Eroor fix

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -38,21 +38,21 @@ const Experience = () => {
                     <br />
                     My expertise in React.js and Next.js has empowered me to
                     craft dynamic and responsive web applications. As a React.js
-                    developer, I've mastered the art of creating fast, scalable
-                    and engaging web pages with a focus on delivering
+                    developer, I&apos;ve mastered the art of creating fast,
+                    scalable and engaging web pages with a focus on delivering
                     exceptional user experiences. My proficiency extends to
                     component-based architecture and effective state management.
-                    In the realm of Next.js, I've honed my skills to develop
-                    cutting-edge web applications with seamless server-side
-                    rendering and optimized performance. This includes creating
-                    efficient and intuitive user interfaces that enhance the
-                    overall user journey. Moreover, my experience as a React.js
-                    and Next.js developer has given me a solid foundation in
-                    navigating the latest trends and technologies in the
-                    ever-evolving field of web development. I am passionate
-                    about leveraging these skills to build innovative and
-                    cross-platform solutions that excel on both the web and
-                    mobile platforms.
+                    In the realm of Next.js, I&apos;ve honed my skills to
+                    develop cutting-edge web applications with seamless
+                    server-side rendering and optimized performance. This
+                    includes creating efficient and intuitive user interfaces
+                    that enhance the overall user journey. Moreover, my
+                    experience as a React.js and Next.js developer has given me
+                    a solid foundation in navigating the latest trends and
+                    technologies in the ever-evolving field of web development.
+                    I am passionate about leveraging these skills to build
+                    innovative and cross-platform solutions that excel on both
+                    the web and mobile platforms.
                 </p>
                 <div className="flex-col flex sm:flex-row ">
                     <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
